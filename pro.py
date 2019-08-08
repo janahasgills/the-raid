ea7ccf6b41fda52013b9ed857810b8fa3e882f47
janahasgills <53916082+janahasgills@users.noreply.github.com>
2019-08-08 12:33:01 -0700

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,19 @@
+import time
+import sys
+from images import *
+def intro():
+    print("████████╗██╗  ██╗███████╗    ██████╗  █████╗ ██╗██████╗")
+    print("╚══██╔══╝██║  ██║██╔════╝    ██╔══██╗██╔══██╗██║██╔══██╗")
+    print("   ██║   ███████║█████╗      ██████╔╝███████║██║██║  ██║")
+    print("   ██║   ██╔══██║██╔══╝      ██╔══██╗██╔══██║██║██║  ██║")
+    print("   ██║   ██║  ██║███████╗    ██║  ██║██║  ██║██║██████╔╝")
+    print("   ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═════╝    ")
+    time.sleep(3)
+    print("Welcome to the raid of Area 51.")
+    print("You have the chance to storm a military base and discover the secrets that are beholden in it.")
+    print("Will you survive and be enlightened, or will you die?")
+    print("Let's find out..")
+    time.sleep(4)
+    print("START")
+    print("Choose your Character!")
+    time.sleep(1)
---
+++ project.py
@@ -0,0 +1,6 @@
+import time
+import sys
+from pro import *
+from game import *
+intro()
+first()
